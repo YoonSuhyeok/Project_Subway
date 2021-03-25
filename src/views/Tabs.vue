a93327935f15c545c1be3b1954d207c57c52ef0d
SMJin <sstaw@naver.com>
2021-03-25 16:26:22 +0900

    add tabs icon

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,27 +3,27 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
-          <ion-label>회원 정보</ion-label>
+          <ion-icon :icon="person" />
+          <ion-label>회원정보</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
+          <ion-icon :icon="layers" />
           <ion-label>조합만들기</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="star" />
           <ion-label>즐겨찾기</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="search" />
           <ion-label>조합보기</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="cellular" />
           <ion-label>순위표</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -33,16 +33,18 @@
 
 <script lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { person, layers, star, search, cellular } from 'ionicons/icons';
 
 export default {
   name: 'Tabs',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      person,
+      layers,
+      star,
+      search,
+      cellular,
     }
   }
 }
