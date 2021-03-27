cb8b541aac529f1f2b784dcb7ad67c4ecc9ca8bd
SMJin <sstaw@naver.com>
2021-03-27 23:53:19 +0900

    make logo page

--- src/views/logo.vue
+++ src/views/logo.vue
@@ -1,18 +1,31 @@
 <template>
     <ion-page>
         <ion-content>
-            <!-- Default -->
-            <ion-button>Default</ion-button>
-            
+            <div class="background">
+                <img src="/assets/img/subway-logo.png" class="logo">
+            </div>
         </ion-content>
     </ion-page>
 </template>
 
-<script>
-import { IonButton } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<style>
+    .logo {
+        width: 100%;
+        height: auto;
+    }
 
-export default defineComponent({
-  components: { IonButton }
-});
+    .background {
+        background-image: url('/assets/img/splash-background.png');
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
+<script>
+// import { IonButton } from '@ionic/vue';
+// import { defineComponent } from 'vue';
+
+// export default defineComponent({
+//   components: { IonButton }
+// });
 </script>
