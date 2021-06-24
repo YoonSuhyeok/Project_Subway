f87c2fba5d94a80479fb1212266caa858d9d424c
SMJin <sstaw@naver.com>
2021-06-25 03:14:34 +0900

    edit tab2 (menu)

--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -1,10 +1,22 @@
 <template>
   <ion-page>
 
-    <ion-header>
+    <ion-header class="ion-no-border">
       <ion-toolbar>
-        <ion-title>
-          <ion-icon name="menu-outline"></ion-icon>
+        <ion-buttons slot="start">
+          <ion-menu-button auto-hide="false"></ion-menu-button>
+        </ion-buttons>
+        <ion-buttons slot="secondary">
+          <ion-button>
+            <ion-icon slot="icon-only" :icon="star"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+        <ion-title style="text-align: center;">
+          <ion-checkbox checked="true" color="dark"></ion-checkbox>
+          <ion-checkbox checked="false" color="dark"></ion-checkbox>
+          <ion-checkbox checked="false" color="dark"></ion-checkbox>
+          <ion-checkbox checked="false" color="dark"></ion-checkbox>
+          <ion-checkbox checked="false" color="dark"></ion-checkbox>          
         </ion-title>
       </ion-toolbar>
     </ion-header>
@@ -18,19 +30,19 @@
         </ion-toolbar>
       </ion-header>
 
-      <ion-slides pager="true" :options="slideOpts">
+      <ion-slides pager="false" :options="slideOpts" style="--bullet-background-active:#111111; --bullet-background:#C4C4C4;">
         <ion-slide>
           <div>
             <h1 class="slide-title">메뉴 선택하기</h1>
 
             <ion-segment value="menu" v-model="selectMenu" >
-                <ion-segment-button value="classic" style="--color-checked:#128D15;">
+                <ion-segment-button value="classic">
                     <ion-label>클래식</ion-label>
                 </ion-segment-button>
-                <ion-segment-button value="freshLight" style="--color-checked:#128D15;">
+                <ion-segment-button value="freshLight">
                     <ion-label>프레쉬&라이트</ion-label>
                 </ion-segment-button>
-                <ion-segment-button value="premium" style="--color-checked:#128D15;">
+                <ion-segment-button value="premium">
                     <ion-label>프리미엄</ion-label>
                 </ion-segment-button>
             </ion-segment>
@@ -212,10 +224,10 @@
             <h1 class="slide-title">야채&소스 선택하기</h1>
 
             <ion-segment value="vegitableSorce" v-model="selectVegitableSource" >
-              <ion-segment-button value="vegitable" style="--color-checked:#128D15;">
+              <ion-segment-button value="vegitable">
                   <ion-label>야채</ion-label>
               </ion-segment-button>
-              <ion-segment-button value="source" style="--color-checked:#128D15;">
+              <ion-segment-button value="source">
                   <ion-label>소스</ion-label>
               </ion-segment-button>
             </ion-segment>
@@ -380,6 +392,32 @@
   .menu-box, .toping-box, .salary-box, .set-box {
     width: 100px;
     height: 100px;
+  }
+
+  .white-circle, .dark-circle {
+    border-radius: 50%;
+    border: black solid 1px;
+    width: 30px;
+    height: 30px;
+    display: flex;
+  }
+
+  ion-checkbox {
+    --border-radius: 50%; 
+    --checkmark-color: dark; 
+    --border-color: #c4c4c4; 
+    --background: #c4c4c4;
+    width: 10px;
+    height: 10px;
+    margin: 5px;
+  }
+
+  ion-segment-button {
+    --color-checked: #111111;
+    --background-hover: none;
+    --color: #c4c4c4;
+    --color-checked: #111111;
+    --indicator-color	: none;
   }
 
 </style>
