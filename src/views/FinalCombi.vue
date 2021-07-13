1cc7297c4838bb912621b97fac38bf9c7b648997
SMJin <sstaw@naver.com>
2021-07-13 16:09:06 +0900

    add Combi.vue

---
+++ src/views/FinalCombi.vue
@@ -0,0 +1,29 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-title class="title">
+                <h5>최종 조합</h5>
+            </ion-title>
+        </ion-header>
+
+        <Combi />
+
+    </ion-page>
+</template>
+
+<style scoped>
+    .title {
+        text-align: center;
+        font-weight: bold;
+        color: #009132;
+    }
+</style>
+
+<script lang="ts">
+  import Combi from '@/components/Combi.vue'
+
+export default  {
+  name: 'Final',
+    components: { Combi },
+}
+</script>
